39d57adbe955363daca6ab6f7982622e3c3f2a38
John Irungu <johnirunguchege2000@gmail.com>
2025-01-15 14:27:01 +0300

    update code

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -24,41 +24,46 @@ const ListPage = async ({
 }: {
   searchParams: { [key: string]: string | string[] };
 }) => {
-  const cat = await getData(searchParams);
+  try {
+    const cat = await getData(searchParams);
 
-  return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-      {/* CAMPAIGN */}
-      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
-        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
-          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
-            Grab up to 50% off on
-            <br /> Selected Products
-          </h1>
-          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
-            Buy Now
-          </button>
+    return (
+      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+        {/* CAMPAIGN */}
+        <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
+          <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+            <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+              Grab up to 50% off on
+              <br /> Selected Products
+            </h1>
+            <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
+              Buy Now
+            </button>
+          </div>
+          <div className="relative w-1/3">
+            <Image src="/woman.png" alt="" fill className="object-contain" />
+          </div>
         </div>
-        <div className="relative w-1/3">
-          <Image src="/woman.png" alt="" fill className="object-contain" />
-        </div>
+        {/* FILTER */}
+        <Filter />
+        {/* PRODUCTS */}
+        <h1 className="mt-12 text-xl font-semibold">
+          {cat?.collection?.name} For You!
+        </h1>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={
+              cat.collection?._id || "00000000-000000-000000-000000000001"
+            }
+            searchParams={searchParams}
+          />
+        </Suspense>
       </div>
-      {/* FILTER */}
-      <Filter />
-      {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">
-        {cat?.collection?.name} For You!
-      </h1>
-      <Suspense fallback={<Skeleton />}>
-        <ProductList
-          categoryId={
-            cat.collection?._id || "00000000-000000-000000-000000000001"
-          }
-          searchParams={searchParams}
-        />
-      </Suspense>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error loading page. Please try again later.</div>;
+  }
 };
 
 export default ListPage;
